f395d744d5672896dcb3f0de21732b20761162e1
Rafael Lopez <lpz.raf@gmail.com>
2018-10-01 22:15:06 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -18,11 +18,17 @@ class School
       
   end
   
+<<<<<<< HEAD
    def sort
      new_roster = {}
      @roster.each do |key, values|
   new_roster[key] =values.sort
 end
 new_roster
+=======
+   def sort(name)
+      @roster[name.sort]
+       binding.pry
+>>>>>>> 81d52d979d351cea5706d69386a32dbe877ae8c5
   end
 end
